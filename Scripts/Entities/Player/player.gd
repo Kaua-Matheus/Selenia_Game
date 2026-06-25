ee31d206be29c8125a9123922120189228126686
Kauã Matheus <kauamatheussilva06@gmail.com>
2026-06-24 23:25:16 -0300

    feat: Added player hud

--- Scripts/Entities/Player/player.gd
+++ Scripts/Entities/Player/player.gd
@@ -3,9 +3,17 @@ extends CharacterBody2D
 
 
 # -- Nodes --
-@onready var attack_hit_box: Area2D = $AttackHitBox
+## --- Animation ---
 @onready var attack_animation: AnimatedSprite2D = $AttackHitBox/AttackAnimation
 @onready var player_animation: AnimatedSprite2D = $PlayerAnimation
+
+## --- HitBox ---
+@onready var attack_hit_box: Area2D = $AttackHitBox
+@onready var body_hit_box_shape: CollisionShape2D = $BodyHitBox/BodyHitBoxShape
+
+## --- Collision Box ---
+@onready var body_collision_shape: CollisionShape2D = $BodyCollisionShape
+
 
 # -- Movement --
 ## -- Direction Vars --
@@ -19,8 +27,9 @@ var hitbox_offset: Vector2
 
 # -- Movement --
 ## --- Move Consts ---
-const BASE_MOVE_SPEED: float = 400
+const BASE_MOVE_SPEED: float = 600
 const BASE_STAMINA: float = 100
+const ACCELERATION: float = 300
 
 ## --- Move Vars ---
 var move_speed: float = BASE_MOVE_SPEED
@@ -40,20 +49,23 @@ enum PlayerState { idle, walk, sprint, crouch, dash, attack, attacked, dead }
 var current_state: PlayerState
 
 # -- Dash --
-var dash_timer: float = 0.2
+var dash_timer: float = DASH_TIMER_BASE
 var dash_direction: Vector2 = Vector2.ZERO
 var dash_speed: int = 700
 
 
 # -- Cooldowns --
 ## --- Const Cooldowns ---
-const DASH_COOLDOWN: float = 3.0
+const DASH_COOLDOWN_BASE: float = 3.0
+const DASH_TIMER_BASE: float = 0.2
+#const INVENCIBILITY_COOLDOWN_BASE = 1
 
 ## --- Var Cooldowns ---
 var dash_cooldown: float = 0.0
 
 
 # -- Invencibility --
+## --- Vars Invencibility ---
 var is_invincible: bool = false
 
 
@@ -73,91 +85,89 @@ func _physics_process(delta: float) -> void:
 # - State Machine -
 ## -- Enter State --
 func enter_idle_state():
-	switch_animation_attack()
 	current_state = PlayerState.idle
 	attack_hit_box.monitoring = false
+	switch_animation_attack()
 
 func enter_walk_state():
-	switch_animation_attack()
 	current_state = PlayerState.walk
 	attack_hit_box.monitoring = false
+	switch_animation_attack()
 
 func enter_sprint_state():
-	switch_animation_attack()
 	current_state = PlayerState.sprint
 	attack_hit_box.monitoring = false
+	switch_animation_attack()
 
 func enter_dash_state():
-	switch_animation_attack()
 	current_state = PlayerState.dash
 	attack_hit_box.monitoring = false
+	is_invincible = true
+	switch_animation_attack()
 
 func enter_crouch_state():
-	switch_animation_attack()
 	current_state = PlayerState.crouch
 	attack_hit_box.monitoring = false
+	switch_animation_attack()
 
 func enter_attack_state():
-	switch_animation_attack()
 	current_state = PlayerState.attack
 	attack_hit_box.monitoring = false
+	switch_animation_attack()
 
 func enter_attacked_state():
-	switch_animation_attack()
 	current_state = PlayerState.attacked
 	attack_hit_box.monitoring = false
+	switch_animation_attack()
 
 func enter_dead_state():
-	switch_animation_attack()
 	current_state = PlayerState.dead
 	attack_hit_box.monitoring = false
+	switch_animation_attack()
 
-
-#func _input(event: InputEvent) -> void:
-	#if event.get_action_strength("Crouch"):
-		#print("Crouch button just pressed")
 
 ## --- States ---
 func idle_state(delta: float) -> void:
-	move()
+	move(delta)
 	if stamina < BASE_STAMINA:
 		stamina += delta * 20
 	
 	if Input.is_action_just_pressed("Crouch"):
-		print("Apertado agachar Idle");
 		enter_crouch_state(); return
 	if Input.is_action_just_pressed("Sprint"):
-		print("Correr no Walk");
 		enter_sprint_state(); return
 	if velocity != Vector2.ZERO:
 		enter_walk_state(); return
 	if Input.get_action_strength("Dash") and dash_cooldown <= 0.0:
 		enter_dash_state(); return
-	if Input.get_action_strength("Attack"):
+	if Input.is_action_just_pressed("Attack"):
 		enter_attack_state();
 
 func walk_state(delta: float) -> void:
-	move()
+	move(delta)
 	if stamina < BASE_STAMINA:
 		stamina += delta * 20
 	
 	if Input.is_action_just_pressed("Crouch"):
-		print("Apertado agachar Walk");
 		enter_crouch_state(); return
 	if Input.is_action_just_pressed("Sprint"):
-		print("Correr no Walk");
 		enter_sprint_state(); return
 	if velocity == Vector2.ZERO:
 		enter_idle_state(); return
 	if Input.get_action_strength("Dash") and dash_cooldown <= 0.0:
 		enter_dash_state(); return
-	if Input.get_action_strength("Attack"):
+	if Input.is_action_just_pressed("Attack"):
 		enter_attack_state();
 
 func sprint_state(delta: float):
-	move()
+	move(delta)
 	move_speed = BASE_MOVE_SPEED * 1.5
 	stamina -= delta * 20
+	
+	if Input.is_action_just_pressed("Attack"):
+		move_speed = BASE_MOVE_SPEED
+		enter_attack_state(); return
+		
 	if Input.is_action_just_released("Sprint") or stamina <= 0:
 		move_speed = BASE_MOVE_SPEED
 		if velocity == Vector2.ZERO:
@@ -166,42 +176,60 @@ func sprint_state(delta: float):
 			enter_walk_state(); return
 
 func crouch_state(delta: float):
-	move()
+	move(delta)
+	
+	body_collision_shape.shape.size = Vector2(20, 20)
+	body_hit_box_shape.shape.size = Vector2(20, 20)
+	
 	move_speed = BASE_MOVE_SPEED / 2
 	if stamina < BASE_STAMINA:
 		stamina += delta * 25
 
 	if Input.is_action_just_pressed("Crouch"):
 		move_speed = BASE_MOVE_SPEED
+		body_collision_shape.shape.size = Vector2(20, 28)
+		body_hit_box_shape.shape.size = Vector2(20, 28)
 		if velocity == Vector2.ZERO:
 			enter_idle_state(); return
 		else:
 			enter_walk_state(); return
 
 func dash_state(delta: float) -> void:
-	move()
+	move(delta)
 	velocity = dash_direction * dash_speed
 	dash_timer -= delta
+	
 	if dash_timer <= 0:
-		dash_cooldown = DASH_COOLDOWN
+		dash_timer = DASH_TIMER_BASE
+		dash_cooldown = DASH_COOLDOWN_BASE
 		is_invincible = false
 		if move_direction == Vector2.ZERO:
 			enter_idle_state(); return
 		else:
 			enter_walk_state(); return
 
-func attack_state() -> void:
-	move()
+func attack_state(delta: float) -> void:
+	move(delta)
 	update_hitbox_offset()
-	if player_animation.animation == "attack" and not player_animation.is_playing():
+	if attack_animation.animation == "attack" and not attack_animation.is_playing():
 		if velocity != Vector2.ZERO:
-			enter_walk_state(); return
+			if Input.is_action_pressed("Sprint"):
+				enter_sprint_state(); return
+			else:
+				enter_walk_state(); return
 		else:
 			enter_idle_state(); return
 
-func attacked_state() -> void:
-	move()
-	pass
+func attacked_state(delta: float, amount: float) -> void:
+	move(delta)
+	if current_state == PlayerState.dead:
+		return
+	if is_invincible:
+		return
+		
+	health = max(health - amount, 0)
+	if health <= 0:
+		enter_dead_state()
 
 func dead_state() -> void:
 	if player_animation.animation == "death" and not player_animation.is_playing():
@@ -217,12 +245,14 @@ func update_state(delta: float) -> void:
 		PlayerState.sprint:  sprint_state(delta);
 		PlayerState.dash:    dash_state(delta);
 		PlayerState.crouch:  crouch_state(delta);
-		PlayerState.attack:  attack_state();
+		PlayerState.attack:  attack_state(delta);
 		PlayerState.dead:    dead_state();
 
+
 # --- Movement ---
-func move():
-	velocity = move_direction * move_speed
+func move(delta: float):
+	print(is_invincible)
+	velocity = velocity.move_toward(move_direction * ACCELERATION, move_speed * delta)
 
 func read_input() -> void:
 	update_direction()
@@ -238,6 +268,7 @@ func update_hitbox_offset() -> void:
 	mouse_direction = (mouse_position - global_position).normalized()
 	attack_hit_box.position = mouse_direction * hitbox_offset.x
 	return
+
 
 # --- Attack ---
 func switch_animation_attack() -> void:
